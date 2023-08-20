549cc6e7c24844b329eebc64f7fca290a99056f8
Matt Pahuta <mhpahuta@gmail.com>
2023-08-20 12:32:23 -0700

    Update nav menu styles, box shadow

--- js/index.js
+++ js/index.js
@@ -1,6 +1,10 @@
 // Navigation
+const header = document.querySelector('.header');
 const navToggle = document.getElementById('mobile-nav-toggle');
 const primaryNav = document.getElementById('primary-navigation');
+
+// const iconHamburger = document.querySelector('.icon-hamburger');
+// const iconClose = document.querySelector('.icon-close');
 
 navToggle.addEventListener('click', () => {
   console.log('clicking nav toggle')
@@ -9,7 +13,27 @@ navToggle.addEventListener('click', () => {
     ? navToggle.setAttribute('aria-expanded', false)
     : navToggle.setAttribute('aria-expanded', true)
 
-
   primaryNav.toggleAttribute('data-visible');
 
+  header.toggleAttribute('data-overlay');
+
+
 })
+
+// navToggle.addEventListener('click', () => {
+//   console.log('clicking nav toggle')
+
+//   if (primaryNav.hasAttribute('data-visible')) {
+//     navToggle.setAttribute('aria-expanded', false);
+//     iconHamburger.setAttribute('aria-hidden', true)
+//   } else {
+//     navToggle.setAttribute('aria-expanded', true)
+//     iconClose.setAttribute('aria-hidden', false)
+//   }
+
+//   primaryNav.toggleAttribute('data-visible');
+
+//   header.toggleAttribute('data-overlay');
+
+
+// })
