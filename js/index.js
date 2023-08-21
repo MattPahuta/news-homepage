2217867178864b9a2625632f85ad20d6b83a9012
Matt Pahuta <mhpahuta@gmail.com>
2023-08-21 15:49:52 -0700

    Switch trending stories section to grid layout, minor style updates

--- js/index.js
+++ js/index.js
@@ -3,11 +3,8 @@ const header = document.querySelector('.header');
 const navToggle = document.getElementById('mobile-nav-toggle');
 const primaryNav = document.getElementById('primary-navigation');
 
-// const iconHamburger = document.querySelector('.icon-hamburger');
-// const iconClose = document.querySelector('.icon-close');
 
 navToggle.addEventListener('click', () => {
-  console.log('clicking nav toggle')
 
   primaryNav.hasAttribute('data-visible') 
     ? navToggle.setAttribute('aria-expanded', false)
@@ -19,21 +16,3 @@ navToggle.addEventListener('click', () => {
 
 
 })
-
-// navToggle.addEventListener('click', () => {
-//   console.log('clicking nav toggle')
-
-//   if (primaryNav.hasAttribute('data-visible')) {
-//     navToggle.setAttribute('aria-expanded', false);
-//     iconHamburger.setAttribute('aria-hidden', true)
-//   } else {
-//     navToggle.setAttribute('aria-expanded', true)
-//     iconClose.setAttribute('aria-hidden', false)
-//   }
-
-//   primaryNav.toggleAttribute('data-visible');
-
-//   header.toggleAttribute('data-overlay');
-
-
-// })
